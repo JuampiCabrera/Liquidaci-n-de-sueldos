3543447770d33851471167dee06c4556f11edbc7
Juampi Cabrera <juampibariloche@gmail.com>
2024-07-31 21:03:08 -0300

    selector

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-class Jornada {
-    constructor (tipoDeJornada,cantidadDeHoras,){
-        this.tipo=tipoDeJornada;
-        this.horas=cantidadDeHoras;
-    }
-}
-const Jornada1= new Jornada (document.getElementById('jornada') ,48);
-const Jornada2 = new Jornada (document.getElementById('jornada') , 32);
-const Jornada3 =new Jornada (document.getElementById('jornada') , 28);
+// class Jornada {
+//     constructor (tipoDeJornada,cantidadDeHoras,){
+//         this.tipo=tipoDeJornada;
+//         this.horas=cantidadDeHoras;
+//     }
+// }
+// const jornada1= new Jornada ("Jornada Completa" ,48);
+// const jornada2 = new Jornada ("Media Jornada" , 32);
+// const jornada3 =new Jornada ("Jornada 3/4", 28);
+
 
 function calcularLiquidacion() {
     let nombre = document.getElementById('nombre').value;
@@ -14,8 +15,10 @@ function calcularLiquidacion() {
     let diasTrabajados = document.getElementById('dias').value;
     let horasPorDia = parseFloat(document.getElementById('horas').value);
     let valorHora = parseFloat(document.getElementById('valorHora').value);
+    let selectedJornadas = document.getElementById('jornadas');
+    let valueTexto = selectedJornadas.options[selectedJornadas.selectedIndex].text;
 
-    if (!nombre || isNaN(horasPorDia) || isNaN(diasTrabajados) || isNaN(valorHora)) {
+    if ( !nombre || isNaN(horasPorDia) || isNaN(diasTrabajados) || isNaN(valorHora)) {
         alert('Por favor complete todos los campos correctamente.');
         return;
     }
@@ -28,6 +31,7 @@ function calcularLiquidacion() {
         <p>Fecha de liquidacion ${fecha}</p>
         <p>Horas totales trabajadas: ${horasTotales.toFixed(2)}</p>
         <p>Sueldo Bruto: $${sueldoBruto.toFixed(2)}</p>
+        <p>Tipo de jornada ${valueTexto}</p>
     `;
     document.getElementById('resultado').innerHTML = resultadoHTML;
 }
@@ -36,3 +40,9 @@ let btnCalcular = document.getElementById("btnCalcular");
 btnCalcular.addEventListener('click',() => {
     calcularLiquidacion()
 })
+
+function guardarDatosLocalStorage(nombre, horasTotales, sueldoBruto) {
+    localStorage.nombre = nombre;
+    localStorage.horasTotales = horasTotales;
+    localStorage.sueldoBruto = sueldoBruto ;
+}
